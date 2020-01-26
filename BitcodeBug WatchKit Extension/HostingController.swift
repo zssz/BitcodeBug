32b5be74ac33aca0e82845a2d76753e2c872b298
Zsombor Szabo <zsombor@gmail.com>
2020-01-26 11:08:47 +0200

    Initial Commit

---
+++ BitcodeBug WatchKit Extension/HostingController.swift
@@ -0,0 +1,14 @@
+//
+//  Created by Zsombor Szabo on 26/01/2020.
+//  Copyright © 2020 IZE. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+import SwiftUI
+
+class HostingController: WKHostingController<ContentView> {
+    override var body: ContentView {
+        return ContentView()
+    }
+}
